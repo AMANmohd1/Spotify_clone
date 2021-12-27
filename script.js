602f67514f1c561679a9cc6b374a402a0e2198cb
AMANmohd1 <mohdaman2001@gmail.com>
2021-12-27 16:00:39 +0530

    added play/pause functionality

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-console.log("Welcome to Spotify")
+console.log("Welcome to Spotify");
+
+let songIndex = 0;
+let songs = [
+    {songName:"Song1",filePath:"",coverPath:""},
+    {songName:"Song1",filePath:"",coverPath:""},
+    {songName:"Song1",filePath:"",coverPath:""},
+    {songName:"Song1",filePath:"",coverPath:""},
+    {songName:"Song1",filePath:"",coverPath:""},
+]
+let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let playBtn = document.getElementById('playBtn')
+//handle play/pause
+
+playBtn.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime <= 0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+
+    }
+})
+
+
+//listen to events
+audioElement.addEventListener('timeupdate',()=>{
+    //update seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100)
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change',()=>{
+    audioElement.currentTime = myProgressBar.value*audioElement.duration/100;
+})
